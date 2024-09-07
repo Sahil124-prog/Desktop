9a25e68418ee0618752c28a258059bbc2cb9d5c6
Sahil124-prog <rajputsahil2207@gmail.com>
2024-09-07 17:27:58 +0530

    hello world code

---
+++ Assignments/Day_2/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Hello World" << n;
+    
+    
+    return 0;
+}
